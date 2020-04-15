d64b5b5b523a2bd1052367e8c7731d2565d60f21
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-04-15 18:40:47 -0300

    Mock do LocalStorage

--- src/__tests__/components/TechList.test.js
+++ src/__tests__/components/TechList.test.js
@@ -1,24 +1,38 @@
 import React from 'react';
 // render cria um dom fake para teste
-import {render, fireEvent} from '@testing-library/react';
+import {render, fireEvent, cleanup } from '@testing-library/react';
 
 import TechList from '~/components/TechList';
 
 // describe para descrição dos testes
 describe('TechList component', () => {
 
+  beforeEach(() => {
+    localStorage.clear();
+  })
+
   //it -> descrever a funcionalidade que se esperá ou comportamento esperado
   it('should be able to add new tech', () => {
-    const { getByText, getByTestId, getByLabelText, debug }  = render(<TechList/>);
+    const { getByText, getByTestId, getByLabelText }  = render(<TechList/>);
 
-    // debug();
     fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
     fireEvent.submit(getByTestId('tech-form'));
-    // debug();
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
     expect(getByLabelText('Tech')).toHaveValue('');
 
   });
 
+  it('should store techs in storage', async () => {
+    let { getByText, getByTestId, getByLabelText }  = render(<TechList/>);
+    fireEvent.change(getByLabelText('Tech'), { target: { value: 'Node.js' } });
+    fireEvent.submit(getByTestId('tech-form'));
+    
+    await cleanup();
+    ({ getByText, getByTestId, getByLabelText } = render(<TechList/>));
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']));
+    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
+
+  });
 });
--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,10 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 // import { Container } from './styles';
 
 export default function TechList() {
   const [techs, setTechs] = useState([]);
   const [newTech, setNewTech] = useState('');
+
+  useEffect(() => {
+    const techs = localStorage.getItem('techs');
+    if(techs){
+      setTechs(JSON.parse(techs));
+    }
+  },[]);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  },[techs]);
 
   function handleAddTech() {
     setTechs([...techs, newTech]);
@@ -13,9 +24,10 @@ export default function TechList() {
 
   return (
     <form data-testid="tech-form" onSubmit={handleAddTech}>
+      {Math.random()}
       <ul data-testid="tech-list">
         {techs.map((tech) => (
-          <li key={tech}>{tech}</li>
+          <li key={new Date()}>{tech}</li>
         ))}
       </ul>
       <label htmlFor="tech">Tech</label>
