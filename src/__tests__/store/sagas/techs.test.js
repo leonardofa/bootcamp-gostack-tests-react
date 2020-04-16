557743f4f733b13b35fa4df22be29c91cd3d8045
Leonardo Ferreira Alves <leonardofa@gmail.com>
2020-04-16 17:15:58 -0300

    Mock do axios

--- src/__tests__/store/sagas/techs.test.js
+++ src/__tests__/store/sagas/techs.test.js
@@ -1,14 +1,29 @@
 import { runSaga } from 'redux-saga';
 import { getTechs } from '~/store/modules/techs/saga';
-import { getTechsSuccess } from '~/store/modules/techs/actions';
+import { getTechsSuccess, getTechsFailure } from '~/store/modules/techs/actions';
+import MockAdapter from 'axios-mock-adapter';
+import api from '~/services/api';
+
+const apiMock = new MockAdapter(api);
 
 describe('Techs sagas', () => {
   it('Should be able to fetch techs', async () => {
     const dispatch = jest.fn();
 
+    apiMock.onGet('techs').reply(200, ['Node.js']);
+
     await runSaga({ dispatch }, getTechs).toPromise();
 
     expect(dispatch).toHaveBeenCalledWith(getTechsSuccess(['Node.js']));
+  });
 
+  it('Should fail when api return error', async () => {
+    const dispatch = jest.fn();
+
+    apiMock.onGet('techs').reply(500);
+
+    await runSaga({ dispatch }, getTechs).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(getTechsFailure());
   });
 });
